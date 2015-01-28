b9084fd0da180233962f3dc9a47984d9e65ec082
davidkeeling <davidakeeling@gmail.com>
2015-01-28 15:00:11 -0700

    Image carousel
    Made a thumbnail image carousel to go along with the Focus Area
    selection

--- createpeace.go
+++ createpeace.go
@@ -1,107 +1,130 @@
 package createpeace
 
 import (
-    "net/http"
-    "html/template"
-    "appengine"
-    "appengine/user"
-    "appengine/datastore"
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"encoding/json"
+	"html/template"
+	"net/http"
+	"time"
 )
 
 type ActOfPeace struct {
-    ProjectName string
-    ProjectDescription string
-    ContactInfo string
-    AreaOfFocus string
+	ProjectName        string
+	ProjectDescription string
+	ContactInfo        string
+	AreaOfFocus        string
+	WhenCreated        time.Time
+}
+
+var funcMap = template.FuncMap{
+	"getFocusAreas":    getFocusAreas,
+	"getFocusAreaJSON": getFocusAreaJSON,
+}
+
+var focusAreaList = map[string]string{
+	"Access to Water and Natural Resources": "jonata-Water-bottle.png",
+	"Advancing Women and Children":          "mother-son-line-art-ArtFavor-Mom-holding-childs-hand.png",
+	"Education and Community Development":   "eleve-posant-une-question.png",
+	"Global Health and Wellness":            "papapishu-Doctor-examining-a-patient.png",
+	"Environmental Sustainability":          "sheikh-tuhin-Save-Environment.png",
+	"Conflict Resolution":                   "cyberscooty-shaking-hands3.png",
+	"Inclusivity and Cooperation":           "tetheredtogether.png",
+	"Human Rights":                          "Human-rights-icon.png",
+	"Alleviation of Extreme Poverty":        "chovynz-Money-Bag-Icon.png",
+	"Weapons Access and Proliferation":      "no-guns.png",
+}
+
+func getFocusAreas() map[string]string {
+	return focusAreaList
+}
+
+func getFocusAreaJSON() (focusAreaJSON string) {
+	theJSON, _ := json.Marshal(focusAreaList)
+	return string(theJSON)
 }
 
 func init() {
- http.HandleFunc("/", index)
- http.HandleFunc("/createact", createact)
- http.HandleFunc("/projectlist", projectlist)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/createact", createact)
+	http.HandleFunc("/projectlist", projectlist)
+
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			c.Warningf("Error logging in: %s", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
 
-func index (w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    u := user.Current(c)
-    if u == nil {
-        url, err := user.LoginURL(c, r.URL.String())
-        if err != nil {
-            c.Warningf("Error logging in: %s", err)
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        w.Header().Set("Location", url)
-        w.WriteHeader(http.StatusFound)
-        return
-    }
-    
-    creationForm, err := template.ParseFiles("createpeacetemplate.html")
-    if err != nil {
-        c.Warningf("Error parsing template file: %s", err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    creationForm.Execute(w, u.String())
+	creationForm := template.Must(template.New("creationForm").Funcs(funcMap).ParseFiles("createpeacetemplate.html"))
+
+	creationForm.ExecuteTemplate(w, "createpeacetemplate.html", u)
 }
 
+func createact(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	newProject := ActOfPeace{
+		ProjectName:        r.FormValue("projectname"),
+		ProjectDescription: r.FormValue("projectdescription"),
+		ContactInfo:        r.FormValue("contactinfo"),
+		AreaOfFocus:        r.FormValue("focusarea"),
+		WhenCreated:        time.Now(),
+	}
 
-func createact (w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    newProject := ActOfPeace{
-        ProjectName: r.FormValue("projectname"),
-        ProjectDescription: r.FormValue("projectdescription"),
-        ContactInfo: r.FormValue("contactinfo"),
-        AreaOfFocus: r.FormValue("focusarea"),
-    }
-    
-    _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "ActOfPeace", ancestorKey(c)), &newProject)
-    if err != nil {
-        c.Warningf("Error saving the project: %s", err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    http.Redirect(w, r, "/projectlist", http.StatusSeeOther)
+	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "ActOfPeace", ancestorKey(c)), &newProject)
+	if err != nil {
+		c.Warningf("Error saving the project: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/projectlist", http.StatusSeeOther)
 }
 
+func projectlist(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	listTemplate, err := template.ParseFiles("projectlisttemplate.html")
+	if err != nil {
+		c.Warningf("Error parsing template file: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-func projectlist (w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    listTemplate, err := template.ParseFiles("projectlisttemplate.html")
-    if err != nil {
-        c.Warningf("Error parsing template file: %s", err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    q := datastore.NewQuery("ActOfPeace").Ancestor(ancestorKey(c)).Order("ProjectName").Limit(50)
-    theActs := make([]ActOfPeace, 0, 50)
-    if _, err := q.GetAll(c, &theActs); err != nil {
-        c.Warningf("Error retrieving projects: %s", err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    if len(theActs) == 0 {
-        blankproject := ActOfPeace{
-            ProjectName: "Your project...",
-            ProjectDescription: "...will be the first!",
-            ContactInfo: "What are you going to do?",
-            AreaOfFocus: "",
-        }
-        var noprojects []ActOfPeace
-        noprojects = append(noprojects, blankproject)
-        listTemplate.Execute(w, noprojects)
-        return
-    }
+	q := datastore.NewQuery("ActOfPeace").Ancestor(ancestorKey(c)).Order("-WhenCreated").Limit(50)
+	theActs := make([]ActOfPeace, 0, 50)
+	if _, err := q.GetAll(c, &theActs); err != nil {
+		c.Warningf("Error retrieving projects: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    listTemplate.Execute(w, theActs)
+	if len(theActs) == 0 {
+		blankproject := ActOfPeace{
+			ProjectName:        "Your project...",
+			ProjectDescription: "...will be the first!",
+			ContactInfo:        "What are you going to do?",
+			AreaOfFocus:        "",
+		}
+		var noprojects []ActOfPeace
+		noprojects = append(noprojects, blankproject)
+		listTemplate.Execute(w, noprojects)
+		return
+	}
+
+	listTemplate.Execute(w, theActs)
 }
 
-
-func ancestorKey (c appengine.Context) *datastore.Key {
-    return datastore.NewKey(c, "OngoingActs", "default_actofpeace", 0, nil)
+func ancestorKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "OngoingActs", "default_actofpeace", 0, nil)
 }
